b34348357932e7926fbb031965a755aa093ead48
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-11-12 07:41:32 +0200

    chore: add install bash script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+python -m pip install --upgrade pip
+
+pip install moviepy==1.0.3
+pip install pydub==0.25.1
+pip install python-dotenv==1.0.1
+pip install srt==3.5.3
+pip install streamlit==1.38.0
+pip install torch==2.4.1+cpu --index-url https://download.pytorch.org/whl/cpu
+pip install transformers==4.45.1
+pip install vosk==0.3.45
